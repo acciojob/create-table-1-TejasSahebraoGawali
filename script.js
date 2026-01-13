198deb50954e140c531ded44e9b0ae7049a35c65
Tejas Sahebrao Gawali <156706283+TejasSahebraoGawali@users.noreply.github.com>
2026-01-13 20:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table = document.querySelector("#sampleTable");
+	// let newRow = document.createElement("tr");
+	// newRow.textContent = "<td>New Cell1</td><td>New Cell2</td>";
+	
+	// table.prepend("newRow");
+
+	const newRow = table.insertRow(0);
+	let cell1 = newRow.insertCell(0);
+	let cell2 = newRow.insertCell(1);
+
+	cell1.textContent = "New Cell1";
+	cell2.textContent = "New Cell2";
 }
