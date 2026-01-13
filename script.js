0a8b4b63a1313807b8566a1d028c8e864939af94
Tejas Sahebrao Gawali <156706283+TejasSahebraoGawali@users.noreply.github.com>
2026-01-13 22:51:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 function insert_Row() {
-    //Write your code here
-	let table = document.querySelector("#sampleTable");
+    let table = document.querySelector("#sampleTable");
 
-	let newRow = document.createElement("tr");
-	let cell1 = document.createElement("td");
-	let cell2 = document.createElement("td");
+    let newRow = document.createElement("tr");
+    let cell1 = document.createElement("td");
+    let cell2 = document.createElement("td");
 
-	cell1.textContent = "New Cell1";
-	cell2.textContent = "New Cell2";
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
 
-	newRow.appendChild(cell1);
-	newRow.appendChild(cell2);
-	
-	table.prepend(newRow);
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
 
-	// const newRow = table.insertRow(0);
-	// let cell1 = newRow.insertCell(0);
-	// let cell2 = newRow.insertCell(1);
-
-	// cell1.textContent = "New Cell1";
-	// cell2.textContent = "New Cell2";
+    table.prepend(newRow);
 }
